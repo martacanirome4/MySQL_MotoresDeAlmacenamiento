ac3568da2f31b432c83f86de21fcda16e0de42ef
Marta <martacaninoromero@gmail.com>
2023-03-13 15:24:01 +0100

    Update tabla_CSV.sql

--- tabla_CSV.sql
+++ tabla_CSV.sql
@@ -1,7 +1,6 @@
 create table tabla_CSV (
 	id INT not NULL,
 	saldo DECIMAL not NULL,
-	activo BOOLEAN not NULL,
 	nombre VARCHAR(50) not NULL,
 	apellido VARCHAR(50) not NULL,
 	fecha_nacim DATE not NULL,
